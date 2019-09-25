7b7a82dafe5d812c7401fe97211f56049c98b34e
Tamires <tamires.lowande@gmail.com>
2019-09-25 00:57:12 +0000

    last activity

---
+++ asyncBreeds.js
@@ -0,0 +1,17 @@
+// asyncBreeds.js
+const fs = require('fs');
+
+const breedDetailsFromFile = function(breed, callback) {
+  console.log('breedDetailsFromFile: Calling readFile...');
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    console.log('Callback: I have the data!');
+    if (!error){
+      callback(data);
+    } else if (error) {
+      callback(undefined);
+    } 
+  });
+}
+
+
+module.exports = breedDetailsFromFile
